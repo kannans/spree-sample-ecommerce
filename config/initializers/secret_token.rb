7d10dda7bec3cc95e0022da198987849621b5f99
kannancpt <kannan.s@changepond.com>
2013-11-27 14:37:54 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myshop::Application.config.secret_key_base = '68d990e65f00b6f26d80a78a33daa93ca04a9747328bb4c396bd69e8baa7baa6b5b5e751a5530b3d1438c64f175725f851a315f110b1605b3294911587bac294'
